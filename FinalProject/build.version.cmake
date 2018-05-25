5258e02fccbabcb2b084e7c4d84bbe23cf43b303
alexazv <alexandre.a512@gmail.com>
2018-05-25 12:21:05 -0300

    accelerometer reading

--- FinalProject/build.version.cmake
+++ FinalProject/build.version.cmake
@@ -1,6 +1,6 @@
 set(AUTO_GEN_MESSAGE "/* File was auto-generated, do not modify! */")
 set(PRJ_MAJOR 0)
-set(PRJ_MINOR 3)
+set(PRJ_MINOR 5)
 
 set(VERSION_FILE "${PROJECT_SOURCE_DIR}/build.version")
 
